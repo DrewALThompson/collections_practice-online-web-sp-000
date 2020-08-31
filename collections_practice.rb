0ac86ad4630346d6f9ab314b329f5c5c7d8b4b25
Drew Thompson <drewallynthompson@gmail.com>
2020-08-31 01:23:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,6 +32,6 @@ array.inject {|sum, n| sum += n}
 end
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| element << "s" if index != 1 else }
+  array.each_with_index.collect {|element, index| element << "s" if index != 1  }
 end
 
