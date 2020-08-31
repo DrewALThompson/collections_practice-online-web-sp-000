45840614015bd4a51b7695786ed01d0f1da27be8
Drew Thompson <drewallynthompson@gmail.com>
2020-08-31 00:39:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,7 +11,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-array.each {|element| element[2] <=> element[1]}
+array.collect{|element| element[2] <=> element[1]}
 end
 
 def reverse_array(array)
